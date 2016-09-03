50f192c65fe9a986a4bcbd79c256cf17a40eb75e
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-03 08:37:56 -0700

    Add AddDocument component.

---
+++ imports/ui/components/add-document.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FormGroup, FormControl, Button } from 'react-bootstrap'
+import { Bert } from 'meteor/themeteorchef:bert'
+import { insertDocument } from '../../api/documents/methods'
+
+const handleInsertDocument = (event) => {
+  event.preventDefault()
+  const title = document.querySelector('[name="title"]')
+  const body = document.querySelector('[name="body"]')
+  if (title.value.trim() !== '' && body.value.trim() !== '') {
+    insertDocument.call({
+      title: title.value,
+      body: body.value,
+    }, (error) => {
+      if (error) {
+        Bert.alert(error.reason, 'danger')
+      } else {
+        Bert.alert('Document added!', 'success')
+      }
+    })
+  } else {
+    Bert.alert('Both and title and body are required.', 'danger')
+  }
+}
+
+export const AddDocument = () => (
+  <form onSubmit={ handleInsertDocument } className="AddDocument">
+    <FormGroup>
+      <FormControl
+        name="title"
+        type="text"
+        placeholder="Type a document title."
+      />
+    </FormGroup>
+    <FormGroup>
+      <FormControl
+        name="body"
+        componentClass="textarea"
+        placeholder="Type a document body."
+      />
+    </FormGroup>
+  </form>
+)
