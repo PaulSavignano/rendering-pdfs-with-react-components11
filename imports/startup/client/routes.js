1921adcaa38ceb09a3bddc02654693542d067eba
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-03 13:23:27 -0700

    Add Documents NavItem

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -9,6 +9,7 @@ import { NotFound } from '../../ui/pages/not-found';
 import { RecoverPassword } from '../../ui/pages/recover-password';
 import { ResetPassword } from '../../ui/pages/reset-password';
 import { Signup } from '../../ui/pages/signup';
+import { Documents } from '../../ui/pages/documents'
 
 const requireAuth = (nextState, replace) => {
   if (!Meteor.loggingIn() && !Meteor.userId()) {
@@ -23,7 +24,9 @@ Meteor.startup(() => {
   render(
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
-        <IndexRoute name="index" component={ Index } onEnter={ requireAuth } />        <Route name="login" path="/login" component={ Login } />
+        <IndexRoute name="index" component={ Index } onEnter={ requireAuth } />
+        <Route name="documents" path="/documents" component={ documents } />
+        <Route name="login" path="/login" component={ Login } />
         <Route name="recover-password" path="/recover-password" component={ RecoverPassword } />
         <Route name="reset-password" path="/reset-password/:token" component={ ResetPassword } />
         <Route name="signup" path="/signup" component={ Signup } />
--- imports/ui/components/public-navigation.js
+++ imports/ui/components/public-navigation.js
@@ -3,12 +3,20 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, NavItem } from 'react-bootstrap';
 
 export const PublicNavigation = () => (
-  <Nav pullRight>
-    <LinkContainer to="signup">
-      <NavItem eventKey={ 1 } href="/signup">Sign Up</NavItem>
-    </LinkContainer>
-    <LinkContainer to="login">
-      <NavItem eventKey={ 2 } href="/login">Log In</NavItem>
-    </LinkContainer>
-  </Nav>
+  <div>
+    <Nav>
+      <LinkContainer to="documents">
+        <NavItem eventKey={ 1 } href="/documents">Documents</NavItem>
+      </LinkContainer>
+    </Nav>
+    <Nav pullRight>
+      <LinkContainer to="signup">
+        <NavItem eventKey={ 2 } href="/signup">Sign Up</NavItem>
+      </LinkContainer>
+      <LinkContainer to="login">
+        <NavItem eventKey={ 3 } href="/login">Log In</NavItem>
+      </LinkContainer>
+    </Nav>
+  </div>
+
 );
